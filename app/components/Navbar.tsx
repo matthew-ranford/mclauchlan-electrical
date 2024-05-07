763823c41f0963f6dda1ae79877de4a033783ac3
Matthew Ranford <matt.ranford16@gmail.com>
2024-05-07 19:03:18 +1200

    Initial start on Navbar component & added to page.tsx, hamburgers imported in layout

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { useState } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const navLinks = [
+    { href: '#home', text: 'Home' },
+    { href: '#services', text: 'Services' },
+    { href: '#meet-the-team', text: 'Meet the Team' },
+    { href: '#gallery', text: 'Gallery' },
+    { href: '#contact', text: 'Contact' },
+  ]
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <>
+      <nav className="md:flex md:justify-between md:items-center text-black ">
+        <div className="flex justify-between items-center ps-4 sm:ps-2 md:ps-6 lg:ps-20 2xl:ps-96">
+          <a href="#home">
+            <Image
+              src="/images/logo.png"
+              width={220}
+              height={85}
+              alt="Company Logo"
+            />
+          </a>
+          <span className="md:hidden pe-4">
+            <button
+              className={`hamburger hamburger--collapse ${
+                isOpen ? 'is-active' : ''
+              }`}
+              type="button"
+              onClick={toggleMenu}
+            >
+              <span className="hamburger-box">
+                <span className="hamburger-inner"></span>
+              </span>
+            </button>
+          </span>
+        </div>
+        <ul
+          className={`md:flex md:items-center text-center gap-5 text-xl pt-5 md:pt-0 pe-0 md:pe-2 lg:pe-20 2xl:pe-96 pb-2 md:pb-0 ${
+            isOpen ? '' : 'hidden'
+          }`}
+        >
+          {navLinks.map((link, index) => (
+            <Link href={link.href} key={index}>
+              <li
+                style={{ listStyle: 'none' }}
+                className="my-4 md:my-0 hover:text-yellow-600 hover:scale-105"
+              >
+                {link.text}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </nav>
+    </>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Space_Grotesk } from 'next/font/google'
 import './globals.css'
+import '../node_modules/hamburgers/dist/hamburgers.css'
 
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] })
 
