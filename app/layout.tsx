bf3d76e62ca556bc634b7b14e260b913eca9b4bf
Matthew Ranford <matt.ranford16@gmail.com>
2024-05-11 17:02:25 +1200

    Added initial shortcut icon to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" type="image/x-icon" href="images/logo.png" />
+      </head>
       <body className={spaceGrotesk.className}>{children}</body>
     </html>
   )
