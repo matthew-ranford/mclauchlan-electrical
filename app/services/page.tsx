b3932b72befeb8e2a79cde2e24a697217984c063
Matthew Ranford <matt.ranford16@gmail.com>
2024-05-13 15:38:24 +1200

    Installed sparkles file in UI, added to services file, initial layout of services file complete

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { LampContainer } from '@/components/ui/lamp'
+import { SparklesCore } from '@/components/ui/sparkles'
 import { motion } from 'framer-motion'
 
 export default function Services() {
@@ -22,7 +23,7 @@ export default function Services() {
               Services
             </motion.h1>
           </LampContainer>
-          <div className="flex justify-center pt-10">
+          <div className="flex justify-center pt-20">
             <div className="max-w-2xl text-xl 2xl:text-2xl ps-8 pe-6">
               <p className="pt-4">
                 McLauchlan Electrical Ltd established 2018 by Sam McLauchlan who
@@ -37,6 +38,176 @@ export default function Services() {
                 We provide a high standard of electrical services. We are
                 reliable and efficient and specialise in the following
                 electrical services.
+              </p>
+            </div>
+          </div>
+          {/* Residential section */}
+          <div className="pt-20 w-full flex flex-col items-center justify-center overflow-hidden rounded-md">
+            <h1 className="text-4xl md:text-5xl lg:text-9xl font-bold text-center text-amber-600 relative z-20">
+              Residential
+            </h1>
+            <div className="w-[40rem] h-40 relative">
+              {/* Gradients */}
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+              {/* Core component */}
+              <SparklesCore
+                id="1"
+                background="transparent"
+                minSize={0.4}
+                maxSize={1}
+                particleDensity={1200}
+                className="w-full h-full"
+                particleColor="#be185d"
+              />
+
+              {/* Radial Gradient to prevent sharp edges */}
+              <div className="absolute inset-0 w-full h-full [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+            </div>
+          </div>
+          <div className="flex justify-center pt-10">
+            <div className="max-w-2xl text-xl 2xl:text-2xl ps-8 pe-6">
+              <ul className="pb-4">
+                Whether you are contemplating a new build or requiring
+                electrical work on your existing property, we are experienced in
+                all residential electrical work and can assist you with the
+                following:
+              </ul>
+              <li className="pt-4">Rewires</li>
+              <li className="pt-4">Renovations</li>
+              <li className="pt-4">Appliance Installation</li>
+              <li className="pt-4">Outdoor Lighting</li>
+              <li className="pt-4">New lighting installations</li>
+              <li className="pt-4">Security camera and alarm installation</li>
+              <li className="pt-4">After hours call outs</li>
+              <li className="pt-4">Electrical faults</li>
+              <li className="pt-4">New builds</li>
+            </div>
+          </div>
+
+          {/* Commercial section */}
+          <div className="pt-20 w-full flex flex-col items-center justify-center overflow-hidden rounded-md">
+            <h1 className="text-4xl md:text-5xl lg:text-9xl font-bold text-center text-amber-600 relative z-20">
+              Commercial
+            </h1>
+            <div className="w-[40rem] h-40 relative">
+              {/* Gradients */}
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+              {/* Core component */}
+              <SparklesCore
+                id="2"
+                background="transparent"
+                minSize={0.4}
+                maxSize={1}
+                particleDensity={1200}
+                className="w-full h-full"
+                particleColor="#052e16"
+              />
+
+              {/* Radial Gradient to prevent sharp edges */}
+              <div className="absolute inset-0 w-full h-full [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+            </div>
+          </div>
+          <div className="flex justify-center pt-10">
+            <div className="max-w-2xl text-xl 2xl:text-2xl ps-8 pe-6">
+              <p className="pt-4">
+                We have extensive experience in commercial installations from
+                main switch boards, shop fit outs, cable tray installations,
+                seismic strengthening and commercial lighting design from base
+                build to fit out.
+              </p>
+              <p className="pt-4">
+                Whether you are a small or large business, we can assist you
+                with your commercial electrical needs.
+              </p>
+            </div>
+          </div>
+
+          {/* Industrial section */}
+          <div className="pt-20 w-full flex flex-col items-center justify-center overflow-hidden rounded-md">
+            <h1 className="text-4xl md:text-5xl lg:text-9xl font-bold text-center text-amber-600 relative z-20">
+              Industrial
+            </h1>
+            <div className="w-[40rem] h-40 relative">
+              {/* Gradients */}
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+              {/* Core component */}
+              <SparklesCore
+                id="3"
+                background="transparent"
+                minSize={0.4}
+                maxSize={1}
+                particleDensity={1200}
+                className="w-full h-full"
+                particleColor="#b91c1c"
+              />
+
+              {/* Radial Gradient to prevent sharp edges */}
+              <div className="absolute inset-0 w-full h-full [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+            </div>
+          </div>
+          <div className="flex justify-center pt-10">
+            <div className="max-w-2xl text-xl 2xl:text-2xl ps-8 pe-6">
+              <p className="pt-4">
+                We repair and maintain industrial machinery. We take care of
+                inspection and testing for preventative maintenance.
+              </p>
+              <p className="pt-4">
+                We can assist with any other industrial requirements such as
+                emergency lighting, cleaning of distribution boards, new
+                installations and more.
+              </p>
+            </div>
+          </div>
+
+          {/* Heat pump section */}
+          <div className="pt-20 w-full flex flex-col items-center justify-center overflow-hidden rounded-md">
+            <h1 className="text-4xl md:text-5xl lg:text-9xl font-bold text-center text-amber-600 relative z-20">
+              Heat Pump Servicing & Installation
+            </h1>
+            <div className="w-[40rem] h-40 relative">
+              {/* Gradients */}
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+              <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+              <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+              {/* Core component */}
+              <SparklesCore
+                id="4"
+                background="transparent"
+                minSize={0.4}
+                maxSize={1}
+                particleDensity={1200}
+                className="w-full h-full"
+                particleColor="#2563eb"
+              />
+
+              {/* Radial Gradient to prevent sharp edges */}
+              <div className="absolute inset-0 w-full h-full [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+            </div>
+          </div>
+          <div className="flex justify-center pt-10">
+            <div className="max-w-2xl text-xl 2xl:text-2xl ps-8 pe-6">
+              <p className="pt-4">
+                We supply and install the highest quality heat pumps. Our heat
+                pump specialists are experts in the industry ensuring that you
+                get the best product for your needs.
+              </p>
+              <p className="pt-4">
+                We also service heat pumps and recommend annual servicing of all
+                heat pumps we install. Call us today for a quote.
               </p>
             </div>
           </div>
