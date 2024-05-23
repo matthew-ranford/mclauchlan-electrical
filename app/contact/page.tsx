7193eaa20f7182799ca38f8694e4291271ace56f
Matthew Ranford <matt.ranford16@gmail.com>
2024-05-23 20:07:42 +1200

    Initial start to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -22,6 +22,103 @@ export default function Contact() {
               Contact
             </motion.h1>
           </LampContainer>
+          <section className="text-gray-600 body-font relative">
+            <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+              <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                <iframe
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                  width="100%"
+                  height="100%"
+                  className="absolute inset-0"
+                  title="map"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001.4855746582207!2d174.92826407521338!3d-41.21118763573824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38aa7d3e8a48a1%3A0xeae07fa10ab5678d!2s17%20Fairfield%20Avenue%2C%20Fairfield%2C%20Lower%20Hutt%205011!5e0!3m2!1sen!2snz!4v1716450058854!5m2!1sen!2snz"
+                  style={{
+                    // filter: 'grayscale(1) contrast(1.2)',
+                    opacity: '0.7',
+                  }}
+                ></iframe>
+                <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+                  <div className="lg:w-1/2 px-6">
+                    <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                      ADDRESS
+                    </h2>
+                    <p className="mt-1">
+                      McLauchlan Eletrical, 17 Fairfield Avenue, Fairfield,
+                      Lower Hutt 5011
+                    </p>
+                  </div>
+                  <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                    <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                      EMAIL
+                    </h2>
+                    <a className="text-amber-600 leading-relaxed">
+                      sam@mclauchlaneletrical.co.nz
+                    </a>
+                    <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
+                      PHONE
+                    </h2>
+                    <p className="leading-relaxed">027 211 0807</p>
+                  </div>
+                </div>
+              </div>
+              <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                <h2 className="text-gray-900 text-xl mb-3 font-medium title-font">
+                  Questions? Fill out the form to contact Sam!
+                </h2>
+                <p className="leading-relaxed mb-2 text-gray-600">
+                  Alternatively, email or call Sam at one of the links below!
+                </p>
+                <p className="leading-relaxed mb-5 text-gray-600">
+                  We will get back to you as soon as possible!
+                </p>
+                <div className="relative mb-4">
+                  <label
+                    htmlFor="name"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="w-full bg-white rounded border border-gray-300 focus:border-amber-600 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+                <div className="relative mb-4">
+                  <label
+                    htmlFor="email"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full bg-white rounded border border-gray-300 focus:border-amber-600 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+                <div className="relative mb-4">
+                  <label
+                    htmlFor="message"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    className="w-full bg-white rounded border border-gray-300 focus:border-amber-600 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                  ></textarea>
+                </div>
+                <button className="text-white bg-amber-600 border-0 py-2 px-6 focus:outline-none hover:bg-amber-700 rounded text-lg">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </section>
         </div>
       </section>
     </>
