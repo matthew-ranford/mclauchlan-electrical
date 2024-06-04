257c0d823ab7c19a8982a31ff9397dd5f931ed3f
Matthew Ranford <matt.ranford16@gmail.com>
2024-06-04 13:21:21 +1200

    Tweaks to padding, margin & navlinks in Footer component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,11 +6,11 @@ import logo from '../../public/images/logo.png'
 
 export default function Footer() {
   const navLinks = [
-    { href: '/', text: 'Home 🏠' },
-    { href: 'services', text: 'Services ⚡' },
-    { href: 'meet-the-team', text: 'Meet the Team 👨🏽‍👨🏽‍👦🏽‍👦🏽' },
-    { href: 'gallery', text: 'Gallery 🖼️ ' },
-    { href: 'contact', text: 'Contact 📞' },
+    { href: '/', text: 'Home' },
+    { href: 'services', text: 'Services' },
+    { href: 'meet-the-team', text: 'Meet the Team' },
+    { href: 'gallery', text: 'Gallery' },
+    { href: 'contact', text: 'Contact' },
   ]
 
   const contactDetails = [
@@ -27,23 +27,20 @@ export default function Footer() {
   return (
     <>
       <div className="mt-20 pt-10 pb-14 bg-stone-800 rounded-2xl border flex flex-col items-center">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-          <div className="md:text-right text-center lg:pe-20 my-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <div className="lg:text-right text-center lg:ps-10 lg:pe-10 my-auto">
             <h2 className="text-2xl 2xl:text-4xl text-amber-600 font-bold pb-6">
               Contact
             </h2>
             <ul className="text-lg 2xl:text-xl text-stone-400 space-y-2">
               {contactDetails.map((link, index) => (
-                <li
-                  key={index}
-                  className="hover:text-sky-900 md:hover:scale-105"
-                >
+                <li key={index} className="hover:text-sky-900">
                   <Link href={link.href}>{link.text}</Link>
                 </li>
               ))}
             </ul>
           </div>
-          <div className="flex justify-center items-center pt-10 lg:pt-0">
+          <div className="flex justify-center items-center pt-10 md:ml-10 lg:pt-0">
             <Image
               src={logo}
               width={275}
@@ -52,17 +49,14 @@ export default function Footer() {
               priority={true}
             />
           </div>
-          <div className="md:text-left text-center lg:ps-20 my-auto">
-            <h2 className="text-2xl 2xl:text-4xl text-amber-600 font-bold pb-6 lg:pt-24">
+          <div className="lg:text-left text-center lg:ps-20 my-auto">
+            <h2 className="text-2xl 2xl:text-4xl text-amber-600 font-bold pb-6 md:pt-20 md:mt-2">
               Navigation
             </h2>
 
             <ul className="text-lg 2xl:text-xl text-stone-400 space-y-2">
               {navLinks.map((link, index) => (
-                <li
-                  key={index}
-                  className="hover:text-amber-600 md:hover:scale-105"
-                >
+                <li key={index} className="hover:text-amber-600">
                   <Link href={link.href}>{link.text}</Link>
                 </li>
               ))}
